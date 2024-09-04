664bf187744a283b4eb513dc26a7e9506d126a03
phuwanat taratipprai <phuwanat@cmu.ac.th>
2024-09-04 11:41:19 +0700

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year ="2024"
+            fullName="Phuwanat Taratipprai"
+            studentId="660610786"
           />
         </div>
       </body>
